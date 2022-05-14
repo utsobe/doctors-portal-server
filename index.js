856120da823c146e9aae3ed83d2ddae60de51845
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-05-15 00:32:27 +0600

    available slots by date API created

--- index.js
+++ index.js
@@ -46,6 +46,35 @@ async function run() {
             }
             const result = await bookingCollection.insertOne(booking);
             return res.send({ success: true, result });
+        });
+
+
+        // WARNING //
+        // this is not the proper way to query
+        // after learning more about mongodb. use aggregate lookup, pipeline , match, group
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'May 14, 2022';
+
+            //step 1: get all services
+
+            const services = await serviceCollection.find().toArray();
+
+            // step 2: get the booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+            // step 3: for each service, find bookings for that service
+            services.forEach(service => {
+                // step 4: find the bookings for that service. output: [{}, {}, {}, {}]
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                // step 5: select slot for the service bookings
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                // step 6: select those slots that are not in bookedSlots
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                // step 7: set available to slots to make it easier
+                service.slots = available;
+            })
+
+            res.send(services);
         })
 
     }
